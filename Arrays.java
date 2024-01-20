9e3766d11179e366aee50cf5b44b4b7613a7aff7
Garth <102332745+ASDF28765@users.noreply.github.com>
2024-01-20 12:34:34 -0500

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Declaring Arrays
+        String[] user_ID = new String[3];
+        user_ID[0] = "568-89";
+        user_ID[1] = "578-34";
+        user_ID[2] = "364-20";
+
+        double[] muns = { 4, 6, 8 };
+        System.out.println("Muns1 is " + muns[0] + ", Muns2 is" + muns[1]);
+        System.out.println("Muns3 is " + muns[2]);
+
+        System.out.println("John user_D is " + user_ID[0]);
+        System.out.println("Mark user_D is " + user_ID[1]);
+        System.out.println("Jean user_D is " + user_ID[2]);
+
+    }
+}
